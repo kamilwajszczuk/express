b916fba999e23ecd4ff91e79763e1a4c3b9e7f61
Kamil Wajszczuk <kamil.wajszczuk@gmail.com>
2019-09-15 20:16:53 +0200

    config

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    db: '',
+    db: 'mongodb+srv://admin:9jeSg*!kBX7.ep$@cluster0-j2tk0.mongodb.net/test?retryWrites=true&w=majority',
     keySession: ['TWOJKLUCZ'],
     maxAgeSessione: 24 * 60 * 60 * 1000
 }
